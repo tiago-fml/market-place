515436eab21536fb63cfac1bf0ce9ea86ba31b9c
Tiago Leite <76402203+tiago-fml@users.noreply.github.com>
2024-08-01 23:33:04 +0100

    Update Program.cs

--- microservices/user-service/Program.cs
+++ microservices/user-service/Program.cs
@@ -1,8 +1,4 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using user_service;
 using user_service.Data;
 
