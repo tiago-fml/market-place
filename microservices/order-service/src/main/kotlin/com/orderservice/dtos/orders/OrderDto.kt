afcab6016c751034138c19d43e845760ad2a7180
Tiago Leite <76402203+tiago-fml@users.noreply.github.com>
2024-08-13 15:09:35 +0100

    Update OrderDto.kt

--- microservices/order-service/src/main/kotlin/com/orderservice/dtos/orders/OrderDto.kt
+++ microservices/order-service/src/main/kotlin/com/orderservice/dtos/orders/OrderDto.kt
@@ -2,7 +2,6 @@ package com.orderservice.dtos.orders
 
 import com.orderservice.dtos.orderlines.OrderLineDto
 import com.orderservice.enums.OrderStatus
-import com.orderservice.models.OrderLine
 import java.time.LocalDateTime
 import java.util.*
 
