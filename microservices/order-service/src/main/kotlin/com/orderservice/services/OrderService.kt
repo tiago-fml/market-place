575d4859e435f447dd1286a176e0222e13f22deb
Tiago Leite <76402203+tiago-fml@users.noreply.github.com>
2024-08-13 15:09:58 +0100

    Update OrderService.kt

--- microservices/order-service/src/main/kotlin/com/orderservice/services/OrderService.kt
+++ microservices/order-service/src/main/kotlin/com/orderservice/services/OrderService.kt
@@ -4,25 +4,25 @@ import com.orderservice.dtos.orders.OrderCreateDto
 import com.orderservice.models.Order
 import com.orderservice.models.OrderLine
 import com.orderservice.repositories.OrderRepository
+import io.netty.handler.codec.AsciiHeadersEncoder.NewlineType
 import org.springframework.stereotype.Service
 
 @Service
 class OrderService(private val orderRepo: OrderRepository) {
     
     fun placeOrder(orderCreateDto: OrderCreateDto):Order {
-        val orderLines = orderCreateDto.orderLines.map { orderLineDto ->
-            OrderLine.fromCreateDto(orderLineDto)
-        }.toList()
-        
         val order = Order(
             userId = orderCreateDto.userId,
             total = orderCreateDto.total,
             shippingAddress = orderCreateDto.shippingAddress,
-            orderLines = orderLines
         )
 
-        orderLines.forEach { it.order = order }
+        val orderLines = orderCreateDto.orderLines.map { orderLineDto ->
+            OrderLine.fromCreateDto(orderLineDto, order)
+        }.toList()
         
+        order.orderLines = orderLines
+
         return orderRepo.save(order)
     }
 }
